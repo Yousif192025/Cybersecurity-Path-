f004a764ba894864adcff815b57f79a9d72c3b80
Yousif192025 <alahli192024@gmail.com>
2025-07-12 14:28:53 +0300

    Update webpack.common.js

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,1 +1,41 @@
- # جديد - إعدادات Webpack
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/scripts/main.js',
+    styles: './src/styles/main.scss'
+  },
+  output: {
+    path: path.resolve(__dirname, '../public'),
+    filename: 'assets/js/[name].[contenthash].js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/templates/index.html',
+      filename: 'index.html',
+      chunks: ['main', 'styles']
+    })
+  ]
+};
