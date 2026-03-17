7e2491d85f4d7d499d388d897356548f943d32b6
Yousif192025 <alahli192024@gmail.com>
2026-03-17 15:16:12 +0300

    Update supabase.js

--- docs/assets/js/core/supabase.js
+++ docs/assets/js/core/supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
+export const supabase = createClient(
+"https://suvpaunulhqfoclepwoz.supabase.co",
+"YOUR_PUBLIC_KEY"
+)
