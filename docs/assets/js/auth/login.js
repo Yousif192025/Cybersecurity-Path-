fe929f7f68c1bf056191cf2ee9cf2099c117d6e5
Yousif192025 <alahli192024@gmail.com>
2026-04-04 18:44:22 +0300

    Update login.js

--- docs/assets/js/auth/login.js
+++ docs/assets/js/auth/login.js
@@ -63,7 +63,7 @@ window.loginWithGoogle = async function() {
         const { error } = await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: { 
-                redirectTo: window.location.origin + '/index.html' 
+                redirectTo: window.location.origin + '/landing.html' 
             }
         });
 
